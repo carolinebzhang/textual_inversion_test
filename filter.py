fbdc47c06a4abc8dd0513e2b89449dacb599fb9b
carolinebzhang <caroline_b_zhang@brown.edu>
2024-03-17 00:22:34 -0400

    removing bad images

--- filter.py
+++ filter.py
@@ -4,6 +4,7 @@ import os
 import shutil
 def catch_image(filepath):
     try: 
+        filepath = "./distinct_angles/distinct_angles/" + filepath
         Image.open(filepath)
     except Exception as e:
         try: 
