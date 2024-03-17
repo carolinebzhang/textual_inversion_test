0b41b09e011657810ab627e44ff1ba8d2a26acb5
carolinebzhang <caroline_b_zhang@brown.edu>
2024-03-17 11:55:09 -0400

    sdkjfh

--- run.sh
+++ run.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 # Request a GPU partition node and access to 1 GPU
-SBATCH -p gpu --gres=gpu:1
+SBATCH -p gpu --gres=gpu:2
 
 # Request 1 CPU core
 SBATCH -n 4
-SBATCH -t 05:00:00
+SBATCH -t 00:30:00
 SBATCH -g 120g
 
 # Load a CUDA module
@@ -14,4 +14,4 @@ conda deactivate
 conda deactivate
 conda activate ldm
 #run script
-python3 main.py --base configs/latent-diffusion/txt2img-1p4B-finetune.yaml -t --actual_resume models/ldm/text2img-large/model.ckpt -n trial --gpus 0, --data_root distinct_angles/distinct_angles/001_150 --init_word front
+python3 main.py --base configs/latent-diffusion/txt2img-1p4B-finetune.yaml -t --actual_resume models/ldm/text2img-large/model.ckpt -n trial --gpus 0,1 --data_root distinct_angles/distinct_angles/001_150 --init_word front
