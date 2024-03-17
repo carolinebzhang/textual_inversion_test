c25e4637fb791824a40aae2e016d34a64bcba2ad
carolinebzhang <caroline_b_zhang@brown.edu>
2024-03-17 09:58:03 -0400

    skjdfh

---
+++ smaller_folder.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# Set the source and destination directories
+source_dir="./distinct_angles/001"
+destination_dir="./distinct_angles/001_2500"
+
+# Create the destination directory if it doesn't exist
+mkdir -p "$destination_dir"
+
+# Move 2500 images from the source directory to the destination directory
+counter=0
+for file in "$source_dir"/*; do
+    if [ $counter -lt 2500 ]; then
+        mv "$file" "$destination_dir"
+        counter=$((counter+1))
+    else
+        break
+    fi
+done
+
+echo "2500 images moved to $destination_dir"
