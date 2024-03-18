c70cfb270b10609119997b3b51162377ec67be02
carolinebzhang <caroline_b_zhang@brown.edu>
2024-03-18 19:36:44 -0400

    sdkjfh

--- run.sh
+++ run.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
 #SBATCH -p gpu --gres=gpu:1 
-#SBATCH -N 1 
 #SBATCH -n 4
 #SBATCH -t 12:00:00
 #SBATCH -m 120g
@@ -15,4 +14,4 @@ conda deactivate
 conda activate ldm
 
 
-python3 main.py --base configs/latent-diffusion/txt2img-1p4B-finetune.yaml -t --actual_resume models/ldm/text2img-large/model.ckpt -n trial --gpus 0, --data_root distinct_angles/distinct_angles/001_150 --init_word front
+python3 main.py --base configs/latent-diffusion/txt2img-1p4B-finetune.yaml -t --actual_resume models/ldm/text2img-large/model.ckpt -n trial --gpus 0, --data_root distinct_angles/distinct_angles/001_150 --init_word front_camera_view
