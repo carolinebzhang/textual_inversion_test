28ffdcefea43cda4cc5bb31fcfb5ec3d30180f4d
carolinebzhang <caroline_b_zhang@brown.edu>
2024-03-18 21:09:49 -0400

    sdkjfh

--- main.py
+++ main.py
@@ -822,6 +822,6 @@ if __name__ == "__main__":
             dst = os.path.join(dst, "debug_runs", name)
             os.makedirs(os.path.split(dst)[0], exist_ok=True)
             os.rename(logdir, dst)
-        if trainer.global_rank == 0:
-            print("main line 826")
+        #if trainer.global_rank == 0:
+        #    print("main line 826")
             #print(trainer.profiler.summary())
