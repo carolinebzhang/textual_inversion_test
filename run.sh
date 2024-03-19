bbb56ded92afee26ae76a09c2e11f8aeeb1ffa14
carolinebzhang <caroline_b_zhang@brown.edu>
2024-03-19 13:05:25 -0400

    sdjkfh:

--- run.sh
+++ run.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 
 #SBATCH -p 3090-gcondo,gpu --gres=gpu:1
+
 #SBATCH -N 1
+
 #SBATCH -n 4
-#SBATCH -t 6:00:00
-#SBATCH --mem=120G
+#SBATCH --mem=120g
+#SBATCH --time=5:00:00
 module load miniconda3/23.11.0s
 module load cuda
 eval "$(command conda 'shell.bash' 'hook' 2> /dev/null)"
