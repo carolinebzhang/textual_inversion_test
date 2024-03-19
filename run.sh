3dc3368e0d49f68e096f4c332ec1367355bfb009
carolinebzhang <caroline_b_zhang@brown.edu>
2024-03-18 21:04:15 -0400

    sdkjfh

--- run.sh
+++ run.sh
@@ -12,6 +12,7 @@ conda deactivate
 conda deactivate
 echo "$(conda env list)"
 conda activate ldm
+source activate ldm
 
 
 python3 main.py --base configs/latent-diffusion/txt2img-1p4B-finetune.yaml -t --actual_resume models/ldm/text2img-large/model.ckpt -n trial --gpus 0, --data_root distinct_angles/distinct_angles/001_150 --init_word front_camera_view
