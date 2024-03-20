0d89a44d6c057c74a1ba9815b0aee38dc63ea770
carolinebzhang <caroline_b_zhang@brown.edu>
2024-03-20 16:58:51 -0400

    changed configs

---
+++ smaller_002.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# Set the source and destination directories
+source_dir="./distinct_angles/distinct_angles/003"
+destination_dir="./distinct_angles/distinct_angles/003_300"
+
+# Create the destination directory if it doesn't exist
+#mkdir -p "$destination_dir"
+
+# Move 2500 images from the source directory to the destination directory
+counter=0
+for file in "$source_dir"/*; do
+    if [ $counter -lt 300 ]; then
+        cp "$file" "$destination_dir"
+        counter=$((counter+1))
+    else
+        break
+    fi
+done
+
+echo "300 images moved to $destination_dir"
